870097faf3246cffe946f206222efd493c85b1c7
haitaa <mustafahaitaa@gmail.com>
2024-09-13 19:50:26 +0300

    Follow Service

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,12 +21,13 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
-        <body className="">
+        <body className={inter.className}>
           <ThemeProvider
             attribute="class"
             forcedTheme="dark"
             storageKey="gamehub-theme"
           >
+            <Toaster theme="light" position="bottom-center" />
             {children}
           </ThemeProvider>
         </body>
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,13 +2,35 @@ import { db } from "./db";
 import { getSelf } from "./auth-service";
 
 export const getRecommended = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  let userId;
 
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        }
+      },
+      orderBy: {
+        createdAt: "desc",
+      }
+    })
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
