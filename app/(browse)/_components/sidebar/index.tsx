c328bb53a2902b667732a97c8977aca69b1b422b
haitaa <mustafahaitaa@gmail.com>
2024-09-22 00:03:04 +0300

    Follow List

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,15 +1,20 @@
 import { getRecommended } from "@/lib/recommended-service";
-import { Recommended, RecommendedSkeleton } from "./recommended";
-import { Toggle } from "./toggle";
+import { getFollowedUsers } from "@/lib/follow-service";
+
 import { Wrapper } from "./wrapper";
+import { Following, FollowingSkeleton } from "./following";
+import { Toggle } from "./toggle";
+import { Recommended, RecommendedSkeleton } from "./recommended";
 
 export const Sidebar = async () => {
   const recommended = await getRecommended();
+  const follows = await getFollowedUsers();
 
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={follows} />
         <Recommended data={recommended} />
       </div>
     </Wrapper>
@@ -19,6 +24,7 @@ export const Sidebar = async () => {
 export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      <FollowingSkeleton />
       <RecommendedSkeleton />
     </aside>
   );
